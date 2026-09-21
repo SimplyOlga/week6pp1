3e34a191e01a5fbc85f027e1547abb0c7f888b6e
zidan2506 <duydan@metropolia.fi>
2026-09-21 14:52:55 +0300

    [iter5] Final Testing ...

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const validator = require('validator');
 
 // Generate JWT
 const generateToken = (_id) => {
@@ -13,6 +14,13 @@ const generateToken = (_id) => {
 // @access  Public
 const signupUser = async (req, res) => {
   const { name, email, password, phone_number, gender, date_of_birth, membership_status } = req.body;
+
+  if (!validator.isEmail(email)) {
+  return res.status(400).json({ error: "Invalid email format" });
+}
+  if (!validator.isStrongPassword(password)) {
+  return res.status(400).json({ error: "Password is too weak" });
+}
 
   try {
     const user = await User.signup(name, email, password, phone_number, gender, date_of_birth, membership_status);

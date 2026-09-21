e53a1b4324258c0b396f4a5d86d0a88b75dd30fb
Olga <olga.gonchar@metropolia.fi>
2026-09-21 14:09:11 +0300

    [iter3] Understand Authentication Logic ...

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -9,17 +9,23 @@ const requireAuth = async (req, res, next) => {
     return res.status(401).json({ error: "Authorization token required" });
   }
 
-//   console.log(authorization);
-//   console.log(authorization.split(" "));
-//   console.log(authorization.split(" ")[0]);
-//   console.log(authorization.split(" ")[1]);
+  console.log(authorization);
+  console.log("gets a token")
+  console.log(authorization.split(" "));
+  console.log("splits by spaces")
+  console.log(authorization.split(" ")[0]);
+  console.log("this takes the first item in the split")
+  console.log(authorization.split(" ")[1]);
+  console.log("takes the second")
 
   const token = authorization.split(" ")[1];
 
   try {
+    console.log(authorization);
     const { _id } = jwt.verify(token, process.env.SECRET);
 
     req.user = await User.findOne({ _id }).select("_id");
+    //^^^^ this finds the user by id but then instead of taking all of the info it only takes whats under id
     next();
   } catch (error) {
     console.log(error);
